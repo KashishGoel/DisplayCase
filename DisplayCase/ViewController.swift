e3b5e8e7b54f984ece0aee075cdf7bd517032247
Kashish Goel <kashish98@hotmail.com>
2016-05-20 20:25:27 -0400

    Initial Commit

---
+++ DisplayCase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DisplayCase
+//
+//  Created by Kashish Goel on 2016-05-20.
+//  Copyright © 2016 Kashish Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
