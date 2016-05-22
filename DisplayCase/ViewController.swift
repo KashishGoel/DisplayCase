21099986155711f9cc5d42746e1cce3882a06986
Kashish Goel <kashish98@hotmail.com>
2016-05-22 17:32:15 -0400

    login screen and functionality complete

--- DisplayCase/ViewController.swift
+++ DisplayCase/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet var fbButton: MaterialButton!
     @IBOutlet var emailTextField: MaterialTextField!
     @IBOutlet var passTextField: MaterialTextField!
-
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -29,61 +29,69 @@ class ViewController: UIViewController {
     
     func loginButtonClicked() {
         let login:FBSDKLoginManager = FBSDKLoginManager.init()
-       
+        
         if (FBSDKAccessToken.currentAccessToken() != nil){
             
             self.performSegueWithIdentifier("showLoggedIn", sender: self)
-        
+            
         }
-        
+            
         else {
             login.logInWithReadPermissions(["email"], fromViewController: self) { (FBSDKLoginManagerLoginResult, error) in
                 if (error != nil) {
-                      
+                    
                     print(error.localizedDescription)}
                 else {
                     
                     let credential = FIRFacebookAuthProvider.credentialWithAccessToken(FBSDKAccessToken.currentAccessToken().tokenString)
                     FIRAuth.auth()?.signInWithCredential(credential, completion: { (user, error) in
                         NSUserDefaults.standardUserDefaults().setValue(user?.uid, forKey: uuidKey)
+                        NSUserDefaults.standardUserDefaults().synchronize()
                         print("uuid is: " +  NSUserDefaults.standardUserDefaults().stringForKey(uuidKey)!)
-                       
-                        
+                        self.performSegueWithIdentifier("showLoggedIn", sender: self)
                     })
                 }
             }
-           
-        
-        }
-       
-        
-        
-        
-        
+       }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     override func viewDidAppear(animated: Bool) {
-        if (FBSDKAccessToken.currentAccessToken() != nil){
-            self.performSegueWithIdentifier("showLoggedIn", sender: self)
-            
-        }
-    }
+        
+         //   print("uuid is: " +  NSUserDefaults.standardUserDefaults().stringForKey(uuidKey)!)
+            super.viewDidAppear(animated)
 
+            if NSUserDefaults.standardUserDefaults().valueForKey(uuidKey) != nil {
+                    self.performSegueWithIdentifier("showLoggedIn", sender: self)
+            }
+    
+            
+        
+    }
+    
     @IBAction func emailButtonPressed(sender: AnyObject) {
         FIRAuth.auth()?.signInWithEmail(emailTextField.text!, password: passTextField.text!, completion: { (user, error) in
             if error != nil {FIRAuth.auth()?.createUserWithEmail(self.emailTextField.text!, password: self.passTextField.text!, completion: { (user, error) in
+                NSUserDefaults.standardUserDefaults().setValue(user?.uid, forKey: uuidKey)
+                NSUserDefaults.standardUserDefaults().synchronize()
+                 print("uuid is: " +  NSUserDefaults.standardUserDefaults().stringForKey(uuidKey)!)
+                
+                    self.performSegueWithIdentifier("showLoggedIn", sender: self)
+               
                 //
             })}
+            else {
+                    self.performSegueWithIdentifier("showLoggedIn", sender: self)
+            }
         })
         
-        self.performSegueWithIdentifier("showLoggedIn", sender: self)
+    
         
     }
-
+    
 }
 
